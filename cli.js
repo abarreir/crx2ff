b4db2c62cc861a1606084de26c5b3a4123f6d24a
Alexandre Barreira <abarreir@gmail.com>
2015-09-01 16:17:03 +0200

    Enabled json reporting through cli

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 
 var extensionChecker = require('./libs/ext-checker');
 var cliReporter = require('./libs/reporters/cli-reporter');
+var jsonReporter = require('./libs/reporters/json-reporter');
 
 var argv = require('minimist')(process.argv.slice(2));
 
@@ -9,10 +10,22 @@ if (!argv.path) {
     return console.error("Provide a path to the extension folder or a crx.");
 }
 
+var reporter = cliReporter;
+
+if (argv.reporter) {
+    if (argv.reporter.match(/^(cli|json)$/) === null) {
+        return console.error("Reporter can only be 'cli' or 'json'.");
+    }
+
+    if (argv.reporter === "json") {
+        reporter = jsonReporter;
+    }
+}
+
 extensionChecker(argv.path, function (error, report) {
     if (error) {
         return console.error(error);
     }
 
-    cliReporter(report);
+    reporter(report);
 });
